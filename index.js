dce942997300564e12ceeae889180f8834d58cc1
Siddharth-Bhardwaj <siddharth.bhardwaj2051999@gmail.com>
2024-04-13 20:51:47 -0400

    upload fix

--- index.js
+++ index.js
@@ -1,66 +1,99 @@
-var apigClient = apigClientFactory.newClient({
-    apiKey : 'beuV2SuzVr6ZqoI6VP0cA4ImJSVEXlEk6vlOtTcx'
+var sdk = sdkFactory.newClient({
+  apiKey: "beuV2SuzVr6ZqoI6VP0cA4ImJSVEXlEk6vlOtTcx",
 });
 
-function search_button(searchText) {
+function triggerSearch(searchText) {
+  document.getElementById("search_input").value = searchText;
+  document.getElementById("search_results").innerHTML =
+    '<h4 style="text-align:center">';
 
-    document.getElementById('search_input').value = searchText;
-    document.getElementById('search_results').innerHTML = "<h4 style=\"text-align:center\">";
+  var params = {
+    keyword: searchText,
+    "x-api-key": "beuV2SuzVr6ZqoI6VP0cA4ImJSVEXlEk6vlOtTcx",
+  };
 
-    var params = {
-        "keyword": searchText,
-        'x-api-key': 'beuV2SuzVr6ZqoI6VP0cA4ImJSVEXlEk6vlOtTcx'
-    };
+  sdk
+    .searchGet(params, {}, {})
+    .then(function (result) {
+      console.log(result);
+      image_paths = result["data"];
 
-    apigClient.searchGet(params, {}, {})
-        .then(function (result) {
-            console.log(result);
-            image_paths = result["data"];
+      var photosDiv = document.getElementById("search_results");
+      photosDiv.innerHTML = "";
 
-            var photosDiv = document.getElementById("search_results");
-            photosDiv.innerHTML = "";
-
-            var n;
-            for (n = 0; n < image_paths.length; n++) {
-                const s3Object = image_paths[n];
-                photosDiv.innerHTML += `<img src="${s3Object}" style="width:25%; margin-top: 10px">`;
-            }
-
-        }).catch(function (result) {
-            console.log(result);
-            var photosDiv = document.getElementById("search_results");
-            photosDiv.innerHTML = "Image not found";
-        });
+      var n;
+      for (n = 0; n < image_paths.length; n++) {
+        const s3Object = image_paths[n];
+        photosDiv.innerHTML += `<img src="${s3Object}" style="width:25%; margin-top: 10px">`;
+      }
+    })
+    .catch(function (result) {
+      console.log(result);
+      var photosDiv = document.getElementById("search_results");
+      photosDiv.innerHTML = "No Images found";
+    });
 }
 
-function photoSearch() {
-    var searchText = document.getElementById('search_input');
-    if (!searchText.value) {
-        alert('Please enter a valid text or voice input!');
-    } else {
-        searchText = searchText.value.trim().toLowerCase();
-        search_button(searchText);
-    }
-    
+function photosearch() {
+  var searchText = document.getElementById("search_input");
+  if (!searchText.value) {
+    alert("Search keywords are required.");
+  } else {
+    searchText = searchText.value.trim().toLowerCase();
+    triggerSearch(searchText);
+  }
+}
+
+function uploadPhoto() {
+  var filePath = document.getElementById("file_input").value.split("\\");
+  var customLabels = document.getElementById("custom_labels").value;
+  var reader = new FileReader();
+  var file = document.getElementById("file_input").files[0];
+  reader.onload = function (event) {
+    body = btoa(event.target.result);
+    var params = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "*",
+      "Content-Type": file.type,
+      bucket: "ccphotoalbum",
+      key: file.name,
+      "x-amz-meta-customLabels": customLabels,
+    };
+    sdk.uploadBucketKeyPut(params, body, {}).catch(function (error) {
+      console.log(error);
+    });
+  };
+  reader.readAsBinaryString(file);
 }
 
 function photoUpload() {
-    let photoFile = document.getElementById('file_input').files[0];
-    let fileReader = new FileReader();
+  let photoFile = document.getElementById("file_input").files[0];
+  let fileReader = new FileReader();
 
-    fileReader.onload = function() {
-        let xhr = new XMLHttpRequest();
-        let inputData = document.getElementById('file_input').files[0];
-        xhr.addEventListener("readystatechange", function () {
-            if (this.readyState === 4) {
-                document.getElementById('uploadText').innerHTML ='Uploaded';
-            }
-        });
-        xhr.open("PUT", "https://c18n9k15l8.execute-api.us-east-1.amazonaws.com/dev/upload/ccphotoalbum/"+inputData.name);
-        xhr.setRequestHeader("Content-Type", inputData.type);
-        xhr.setRequestHeader("x-api-key","beuV2SuzVr6ZqoI6VP0cA4ImJSVEXlEk6vlOtTcx");
-        xhr.setRequestHeader("x-amz-meta-customLabels", custom_labels.value.toString());
-        xhr.send(inputData);
-    };
-    fileReader.readAsArrayBuffer(photoFile);
+  fileReader.onload = function () {
+    let xhr = new XMLHttpRequest();
+    let inputData = document.getElementById("file_input").files[0];
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+        document.getElementById("uploadText").innerHTML =
+          "Uploaded, please wait a few minutes for it to be indexed.";
+      }
+    });
+    xhr.open(
+      "PUT",
+      "https://c18n9k15l8.execute-api.us-east-1.amazonaws.com/dev/upload/ccphotoalbum/" +
+        inputData.name
+    );
+    xhr.setRequestHeader("Content-Type", inputData.type);
+    xhr.setRequestHeader(
+      "x-api-key",
+      "beuV2SuzVr6ZqoI6VP0cA4ImJSVEXlEk6vlOtTcx"
+    );
+    xhr.setRequestHeader(
+      "x-amz-meta-customLabels",
+      custom_labels.value.toString()
+    );
+    xhr.send(inputData);
+  };
+  fileReader.readAsArrayBuffer(photoFile);
 }
